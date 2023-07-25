ca7a47c12813d216e54fd9928e13e0c2a7d0222b
jiangxiaoxv <1043479536@qq.com>
2023-07-25 15:55:45 +0800

    feat: typescript 学习

---
+++ src/11.shell开发.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+# whoami;
+# date;ls;cd /tmp/;cd ~;cd -
+
+# echo "当前时间日期是：`date`"
+
+# echo命令，在linux中的格式化
+echo "the time and date are: "
+date
+
+echo ''
+echo "let's see who's login into the system: `whoami`"
+
+# source 或者 . 是在当前shell环境中执行
+# sh 或者sh 去执行脚本的时候，是开启子shell运行的，变量也是在子shell环境中加载，子shell退出后，变量也就消失了
+# 对变量的引用一定加上符号
+
+# linux , shell变量的替换引用
+# shell 一大特性，就是可以从命令的执行结果中，再提取结果，因此特别适合便携脚本
+  # $()
+  # `` 反引号
+
+
+
